0eece2fefe9cd0a6a3d6319acf9411736c89cf65
guilhaume bordiau <github@guilhaume.fr>
2024-02-03 20:21:10 +0100

    Fixed delay not working + added ETA + vanilla javascript (no jQuery)

--- SteamLicenseRemover.js
+++ SteamLicenseRemover.js
@@ -1,42 +1,39 @@
 // ==UserScript==
     // @name         Steam License Remover
     // @namespace
-    // @version      1.1
-    // @description  Remove any "Free" games from your Steam Library by removing the game's license from your account.  
+    // @version      1.2
+    // @description  Remove any "Free" games from your Steam Library by removing the game's license from your account.
     // @author       IroN404
     // @match        https://store.steampowered.com/account/licenses/
 
 
-const table = document.querySelector('.account_table');
-const rows = table.querySelectorAll('tr');
-const total = rows.length;
+const removeLinkEls = document.querySelectorAll('.free_license_remove_link > a');
+let itemIds = [];
+
+removeLinkEls.forEach(el => itemIds.push(el.getAttribute('href').match(/\d+/)[0]));
+
+const total = itemIds.length;
+console.log(`Starting removal of ${total} entries`);
+const start = Date.now();
 
 let removed = 0;
-const intervalId = setInterval(() => {
-    rows.forEach(row => {
-        const div = row.querySelector('.free_license_remove_link');
-        if(div) {
-            const removeLink = div.querySelector('a');
-            if (removeLink) {
-                removed += 1;
-                const link = removeLink.getAttribute('href');
-                const id = link.split('(')[1].split(',')[0].trim();
-                jQuery.post(
-                    'https://store.steampowered.com/account/removelicense',
-                    {
-                        sessionid: g_sessionID,
-                        packageid: id
-                    }
-                );
-            }
-        }
+const intervalId = setInterval(async () => {
+    const currentId = itemIds.pop();
+    const response = await fetch('https://store.steampowered.com/account/removelicense', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: `sessionid=${g_sessionID}&packageid=${currentId}`
     });
+    if (!response.ok) console.error(response);
 
-    if (removed === total) {
-        console.log(`All ${total} licenses removed!`);
+    removed++;
+
+    if (!itemIds.length) {
+        console.info(`All ${total} licenses removed!`);
         clearInterval(intervalId);
-    }
-    if (removed < total) {
-        console.log(`Removed ${removed} of ${total} licenses.`);
+    } else {
+        const now = Date.now();
+        const remaining = Math.floor((now - start) / removed * (total - removed));
+        console.info(`Removed ${removed} of ${total} licenses. ETA: ${new Date(now + remaining).toLocaleTimeString()}`);
     }
 }, 2000);
